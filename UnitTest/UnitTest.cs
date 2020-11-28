bab4f5356286c25a19a3c8c1960fa604aeb521be
Bruce Wayne <admin@bige0.com>
2020-11-28 23:08:30 +0800

    Add unit testing

---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RunAtStartup;
+using RunAtStartup.Enums;
+using System.Security;
+
+namespace UnitTest
+{
+	[TestClass]
+	public class UnitTest
+	{
+		[TestMethod]
+		public void CurrentUserTest()
+		{
+			const string key = @"RunAtStartup.CurrentUserTest";
+			var service = new StartupService(key);
+			Assert.IsFalse(service.Check());
+
+			service.Set(@"114514");
+			Assert.IsTrue(service.Check());
+
+			service.Delete();
+			Assert.IsFalse(service.Check());
+		}
+
+		[TestMethod]
+		public void LocalMachineTest()
+		{
+			const string key = @"RunAtStartup.LocalMachineTest";
+			var service = new StartupService(key, StartupType.LocalMachine);
+			Assert.IsFalse(service.Check());
+			Assert.ThrowsException<SecurityException>(() =>
+			{
+				service.Set(@"1919810");
+			});
+			Assert.ThrowsException<SecurityException>(() =>
+			{
+				service.Delete();
+			});
+		}
+	}
+}
